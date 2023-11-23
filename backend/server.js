df4262e831e4a6131677bcdf71ffc5ed257b2529
Aaron Sala <salaaron2@gmail.com>
2023-11-23 23:16:10 +0300

    more on backend

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,10 @@ app.get("/", (req, res) => {
 
 app.use("/api/user", userRoutes);
 
+// app.use(notFound)
+// app.use(errorHandler)
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(5000, console.log(`server started on port ${PORT}`.yellow.bold));
